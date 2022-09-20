8292d372309195ac519ce700d27ebc256c3c0f22
Alessandro Macedo <aleluiz73@gmail.com>
2022-09-19 22:04:41 -0300

    Criando componentes
    - Button
    - Input
    - Textarea
    - Note ( em processo )

---
+++ RocketMovies/src/components/Input/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  
+  width: 100%;
+  height: 56px;
+
+  padding-left: 16px ;
+  border: none;
+  border-radius: 10px;
+
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  gap: 16px;
+  
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+  color: ${({ theme }) => theme.COLORS.GRAY_200};
+
+  > input{
+    width: 100%;
+
+    border: none;
+    justify-content: center;
+    
+    background: transparent;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+
+    &::placeholder{
+      color: ${({ theme }) => theme.COLORS.GRAY_200};
+    }
+  }
+`
--- RocketMovies/src/pages/SignIn/index.jsx
+++ RocketMovies/src/pages/SignIn/index.jsx
@@ -1,9 +1,15 @@
 import { Container } from './styles';
 
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+import { Textarea  } from '../../components/Textarea'
+
+import { FiMail } from "react-icons/fi";
+
 export function SignIn(){
   return(
     <Container>
-
+      <Textarea  placeholder="Observações" />
     </Container>
   )
 }
